2158d3c5d238ffb3dd111ea773c727c5a553a439
neeye0n <61579441+neeye0n@users.noreply.github.com>
2024-09-28 01:48:51 +0800

    try to deploy

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,16 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as dotenv from 'dotenv';
-declare const module: any;
 dotenv.config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(process.env.PORT || 3000);
-
-  if (module.hot) {
-    module.hot.accept();
-    module.hot.dispose(() => app.close());
-  }
 }
 bootstrap();
