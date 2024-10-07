8112630252513008084eda2c370b468e4fff1c50
neeye0n <61579441+neeye0n@users.noreply.github.com>
2024-10-08 04:25:21 +0800

    test for phone viewing

--- hunns/src/routes/+page.svelte
+++ hunns/src/routes/+page.svelte
@@ -9,6 +9,7 @@
 	let fancy = 'fancy'.split('');
 </script>
 
+<meta name="viewport" content="width=device-width, initial-scale=0.6" />
 <main>
 	<Parallax sections={5} bind:this={parallax} {disabled}>
 		<ParallaxLayer rate={0.1} class="!bg-transparent flex flex-col justify-center items-center">
@@ -29,52 +30,53 @@
 			rate={0}
 			class="bg-secondary-200 flex flex-col justify-center items-center"
 		>
-			<div class="grid grid-cols-2 place-items-center h-2/5">
-				<div class="card !bg-transparent w-4/5 h-4/5 text-right text-primary-500">
+			<div class="flex justify-between h-2/5">
+				<div
+					class="card !bg-transparent w-4/5 h-4/5 text-right text-primary-500 text-ellipsis overflow-hidden p-10"
+				>
 					<div class="card-header pr-0 pt-0 pb-5">
-						<h2 class="text-2xl font-extrabold tracking-wider">THE BRIDE</h2>
-						<p class="tracking-widest font-extralight text-xl">SARRIE JOY PEÑA</p>
+						<h2 class="text-3xl font-extrabold tracking-wider">THE BRIDE</h2>
+						<p class="tracking-widest font-extralight text-2xl">SARRIE JOY PEÑA</p>
 					</div>
 					<div class="p4">
-						<p class="text-md tracking-wide">
+						<p class="text-md font-light tracking-wide">
 							Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec ullamcorper metus in
 							tortor suscipit viverra. Nulla id turpis condimentum sem accumsan lacinia. Fusce non
 							iaculis nulla. Sed hendrerit eleifend placerat. Curabitur vel cursus ex. Morbi in erat
 							gravida risus pellentesque sollicitudin. Ut et lorem felis. Morbi risus tortor, cursus
-							a finibus non, feugiat ut orci. In elementum nulla sed pharetra hendrerit. Donec
-							vehicula volutpat nunc, quis imperdiet nibh dapibus id.
+							a finibus non, feugiat ut orci.
 						</p>
 					</div>
 				</div>
-				<div class="card !bg-transparent w-4/5 h-4/5 text-left text-primary-500">
+				<div
+					class="card !bg-transparent w-4/5 h-4/5 text-left text-primary-500 text-ellipsis overflow-hidden p-10"
+				>
 					<div class="card-header pl-0 pt-0 pb-5">
-						<h2 class="text-2xl font-extrabold tracking-wider">THE GROOM</h2>
-						<p class="tracking-widest font-extralight text-xl">MARC JOEMAR ESTIGOY</p>
+						<h2 class="text-3xl font-extrabold tracking-wider">THE GROOM</h2>
+						<p class="tracking-widest font-extralight text-2xl">MARC JOEMAR ESTIGOY</p>
 					</div>
 					<div class="p4">
-						<p class="text-md tracking-wide">
+						<p class="text-md font-light tracking-wide">
 							Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec ullamcorper metus in
 							tortor suscipit viverra. Nulla id turpis condimentum sem accumsan lacinia. Fusce non
 							iaculis nulla. Sed hendrerit eleifend placerat. Curabitur vel cursus ex. Morbi in erat
 							gravida risus pellentesque sollicitudin. Ut et lorem felis. Morbi risus tortor, cursus
-							a finibus non, feugiat ut orci. In elementum nulla sed pharetra hendrerit. Donec
-							vehicula volutpat nunc, quis imperdiet nibh dapibus id. Donec vehicula volutpat nunc,
-							quis imperdiet nibh dapibus id.
+							a finibus non, feugiat ut orci.
 						</p>
 					</div>
 				</div>
 			</div>
 			<div class="grid place-items-center">
-				<img class="h-[35px]" src="./heart.png" alt="Joemar And Sarrie" />
+				<img class="h-[40px]" src="./heart.png" alt="Joemar And Sarrie" />
 			</div>
 			<div class="grid h-2/5 w-1/2 place-content-center justify-center">
 				<div class="card !bg-transparent h-4/5 text-primary-500 text-center">
 					<div class="card-header pb-5">
-						<h2 class="text-2xl font-extrabold tracking-wider">OUR STORY</h2>
-						<p class="tracking-widest font-extralight text-xl">JOEMAR + SARRIE</p>
+						<h2 class="text-3xl font-extrabold tracking-wider">OUR STORY</h2>
+						<p class="tracking-widest font-extralight text-2xl">JOEMAR + SARRIE</p>
 					</div>
 					<div class="p4">
-						<p class="text-md tracking-wide">
+						<p class="text-md font-light tracking-wide">
 							Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec ullamcorper metus in
 							tortor suscipit viverra. Nulla id turpis condimentum sem accumsan lacinia. Fusce non
 							iaculis nulla. Sed hendrerit eleifend placerat. Curabitur vel cursus ex. Morbi in erat
